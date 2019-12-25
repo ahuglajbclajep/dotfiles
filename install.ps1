867a7645030be18ab597dd7cded0e3d73e4b1a4e
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-12-26 01:10:06 +0900

    update

--- install.ps1
+++ install.ps1
@@ -1,25 +1,27 @@
 # powershell -executionpolicy remotesigned .\install.ps1
 
 $ErrorActionPreference = "Stop"
-Push-Location "$PSScriptRoot"
 trap { Pop-Location }
+Push-Location "$PSScriptRoot"
+
+# see https://devblogs.microsoft.com/scripting/check-for-admin-credentials-in-a-powershell-script/
+$isadmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
+    [Security.Principal.WindowsBuiltInRole] "Administrator")
 
 $mappings = @(
-    @{ 'from' = '.config\Code\User\settings.json'; 'to' = "$env:APPDATA\Code\User\settings.json" }
+    @{ 'from' = '.config\Code\User\settings.json'; 'to' = "$env:APPDATA\Code\User\settings.json" },
     @{ 'from' = 'other\profiles.json'; 'to' = '' }
 )
 
-try {
+if ($isadmin) {
     foreach ($m in $mappings) {
         New-Item -ItemType Directory -Path (Split-Path -Parent $m.to) -Force > $null
-        New-Item -ItemType SymbolicLink -Path $m.to -Value $m.from -Force -Confirm > $null
+        New-Item -ItemType SymbolicLink -Path $m.to -Value $m.from -Force -cf > $null
     }
 }
-catch [UnauthorizedAccessException] {
-    $ErrorActionPreference = "SilentlyContinue"
-}
+else { }
 
 # for vscode
-if (get-command code -ea stop) { }
+if (Get-Command 'code' -ea SilentlyContinue) { }
 
 Pop-Location
