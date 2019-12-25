30b7e153ad59599b56ccaf1e50b4e9a0e0a9e81d
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-12-26 00:40:46 +0900

    wip

--- install.ps1
+++ install.ps1
@@ -2,21 +2,24 @@
 
 $ErrorActionPreference = "Stop"
 Push-Location "$PSScriptRoot"
+trap { Pop-Location }
 
 $mappings = @(
-    @{ 'from' = '.config\Code\User\settings.json'; 'to' = "$env:APPDATA\Code\User\settings.json" },
+    @{ 'from' = '.config\Code\User\settings.json'; 'to' = "$env:APPDATA\Code\User\settings.json" }
     @{ 'from' = 'other\profiles.json'; 'to' = '' }
 )
 
 try {
     foreach ($m in $mappings) {
-        New-Item -ItemType Directory -Path (Split-Path -Parent $m.to) -Force
-        New-Item -ItemType SymbolicLink -Path $m.to -Value $m.from -Force -Confirm
+        New-Item -ItemType Directory -Path (Split-Path -Parent $m.to) -Force > $null
+        New-Item -ItemType SymbolicLink -Path $m.to -Value $m.from -Force -Confirm > $null
     }
 }
-catch [UnauthorizedAccessException] { }
+catch [UnauthorizedAccessException] {
+    $ErrorActionPreference = "SilentlyContinue"
+}
 
 # for vscode
 if (get-command code -ea stop) { }
 
-trap { Pop-Location }
+Pop-Location
