728e9d33b44a1f94c8ef2190ab17fed38cfef7a6
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-12-26 17:43:24 +0900

    add massage

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -e
 
 cd "$(dirname "$0")"
-files=$(find . -type f ! -path '*/.git/*' ! -path '*/other/*' ! -name 'install.sh' ! -name 'README.md')
-IFS=$'\n'
-for file in $files; do
-  target=~/${file#./}
-  mkdir -p "${target%/*}"
-  ln -is "$PWD/${file#./}" "$target"
-done
-. ~/.profile
+
+read -rp 'create symbolic links? (y/N): ' yn
+if [ "$yn" = 'y' ]; then
+  files=$(find . -type f ! -path '*/.git/*' ! -path '*/other/*' ! -name 'install.sh' ! -name 'README.md')
+  IFS=$'\n'
+  for file in $files; do
+    target=~/${file#./}
+    mkdir -p "${target%/*}"
+    ln -is "$PWD/${file#./}" "$target"
+  done
+  . ~/.profile
+fi
 
 read -rp 'install VS Code extensions? (y/N): ' yn
 if [ "$yn" = 'y' ] && type code > /dev/null 2>&1; then
