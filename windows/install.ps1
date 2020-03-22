4d22ac86c01eb80bab7cac90055b072301bc48c2
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-03-22 19:36:01 +0900

    update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -6,30 +6,36 @@ $ErrorActionPreference = "Stop"
         ForEach-Object { $_ -replace '^#', '' }))) | Set-Content '..\vscode\_extensions.txt' -Encoding Byte
 
 # see https://devblogs.microsoft.com/scripting/check-for-admin-credentials-in-a-powershell-script/
-$isadmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
-  [Security.Principal.WindowsBuiltInRole] "Administrator")
+$isadmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
+  [Security.Principal.WindowsBuiltInRole]"Administrator")
 
 $wt = "$env:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe"
 $wtdir = Get-ChildItem "$env:LOCALAPPDATA\Packages" | Where-Object Name -match 'WindowsTerminal' |
   Select-Object -ExpandProperty 'FullName'
+$bin = "$HOME\app\bin"
 
 $mappings = @(
-  @{ 'name' = 'code'; 'from' = '..\vscode\settings.json'; 'to' = "$env:APPDATA\Code\User\settings.json" },
-  @{ 'name' = 'wt'; 'from' = 'profiles.json'; 'to' = "$wtdir\LocalState\profiles.json" }
+  @{ 'name' = 'vscode'; 'from' = '..\vscode'; 'to' = "$env:APPDATA\Code\User" },
+  @{ 'name' = 'terminal'; 'from' = 'terminal'; 'to' = "$wtdir\LocalState" }
 )
 
 
 $yn = Read-Host 'create symbolic links? (y/N)'
 if ($yn -eq 'y') {
+  # powershell 5.x requires an administrator privilege to execute the `ln` command
   if (!$isadmin) {
     Write-Host 'creating a symbolic link on Windows requires elevation as administrator.' -ForegroundColor Red
   } else {
     foreach ($m in $mappings) {
-      if (($m.name -eq 'wt') -and (!$wtdir)) { continue }
+      # if Windows Terminal is not installed, do nothing because the path of the configuration directory is unknown
+      if (($m.name -eq 'terminal') -and (!$wtdir)) { continue }
 
-      New-Item -ItemType Directory -Path (Split-Path -Parent $m.to) -Force > $null
-      # see https://docs.microsoft.com/powershell/module/microsoft.powershell.core/about/about_commonparameters#confirm
-      New-Item -ItemType SymbolicLink -Path $m.to -Value $m.from -Force -cf:(Test-Path $m.to) > $null
+      foreach ($file in Get-ChildItem $m.from -Recurse -File -Exclude '_*' -Name) {
+        $target = "$($m.to)\$file"
+        New-Item -ItemType Directory -Path (Split-Path -Parent $target) -Force > $null
+        # see https://docs.microsoft.com/powershell/module/microsoft.powershell.core/about/about_commonparameters#confirm
+        New-Item -ItemType SymbolicLink -Path $target -Value "$($m.from)\$file" -Force -cf:(Test-Path $target) > $null
+      }
     }
   }
 }
@@ -37,7 +43,6 @@ if ($yn -eq 'y') {
 $yn = Read-Host 'add "Open with Terminal" into right-click context menu? (y/N)'
 if ($yn -eq 'y') {
   $targets = @('Directory', 'Directory\Background')
-
   foreach ($t in $targets) {
     New-Item -Path "HKCU:\Software\Classes\$t\shell\MyTerminal" -Value 'Open with Terminal' -Force > $null
     New-Item -Path "HKCU:\Software\Classes\$t\shell\MyTerminal\command" -Value "$wt -d `"%V`"" -Force > $null
@@ -46,8 +51,7 @@ if ($yn -eq 'y') {
 
 $yn = Read-Host 'install wslgit? (y/N)'
 if ($yn -eq 'y') {
-  $bin = "$HOME\app\bin"
-
+  # add ~\app\bin to the `$env:Path`
   # see https://stackoverflow.com/questions/714877/setting-windows-powershell-environment-variables
   $path = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::User) -replace ';$', ''
   if (($path -split ';') -notcontains $bin) {
@@ -63,7 +67,7 @@ if ($yn -eq 'y') {
 
 $yn = Read-Host 'install VS Code extensions? (y/N)'
 if (($yn -eq 'y') -and (Get-Command 'code' -ea SilentlyContinue)) {
-  foreach ($e in Get-Content '..\vscode\_extensions.txt') {
-    code --install-extension "$e"
+  foreach ($extension in Get-Content '..\vscode\_extensions.txt') {
+    code --install-extension "$extension"
   }
 }
